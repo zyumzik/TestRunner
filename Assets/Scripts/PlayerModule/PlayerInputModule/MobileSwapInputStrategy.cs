b404ba2548bde8f77028f99f77b67a3178745c34
Danil <danil.zyumzik@gmail.com>
2025-06-01 17:17:14 +0300

    Fixed mobile input for player

--- Assets/Scripts/PlayerModule/PlayerInputModule/MobileSwapInputStrategy.cs
+++ Assets/Scripts/PlayerModule/PlayerInputModule/MobileSwapInputStrategy.cs
@@ -6,34 +6,51 @@ namespace PlayerModule.PlayerInputModule
     public class MobileSwapInputStrategy : IPlayerInputStrategy
     {
         private Vector2 _start;
-        private Vector2 _end;
         private Vector2 _direction;
-        
+        private bool _swipeDetected;
+
         public Vector2 GetInputDirection()
         {
-            if (Input.touchCount == 0) return Vector2.zero;
-            
+            if (Input.touchCount == 0)
+            {
+                if (_swipeDetected)
+                {
+                    _swipeDetected = false;
+                    return _direction;
+                }
+
+                return Vector2.zero;
+            }
+
             var touch = Input.GetTouch(0);
+
             switch (touch.phase)
             {
                 case TouchPhase.Began:
                     _start = touch.position;
                     break;
-                case TouchPhase.Moved:
-                    break;
-                case TouchPhase.Stationary:
-                    break;
+
                 case TouchPhase.Ended:
-                    _end = touch.position;
-                    _direction = (_end - _start).normalized;
+                    var end = touch.position;
+                    var delta = end - _start;
+
+                    if (delta.magnitude < 50f) return Vector2.zero;
+
+                    _direction = GetSwipeDirection(delta);
+                    _swipeDetected = true;
                     break;
-                case TouchPhase.Canceled:
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
             }
 
-            return _direction;
+            return Vector2.zero;
+        }
+
+        private Vector2 GetSwipeDirection(Vector2 delta)
+        {
+            if (Mathf.Abs(delta.x) > Mathf.Abs(delta.y))
+                return delta.x > 0 ? Vector2.right : Vector2.left;
+            else
+                return delta.y > 0 ? Vector2.up : Vector2.down;
         }
     }
+
 }
